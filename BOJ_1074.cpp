66730926558aff7b0092b6c6d07dc910977320ce
zxcv859500 <zxcv859500@naver.com>
2021-01-08 08:06:04 +0900

    BOJ_1074 분할정복 최적화

--- BOJ_1074.cpp
+++ BOJ_1074.cpp
@@ -4,32 +4,38 @@
 using namespace std;
 
 int pow(int n) {
-    int result = 2;
+    int result = 1;
     for (int i = 0; i < n; i++) {
         result *= 2;
     }
     return result;
 }
 
-int count = 0;
+int r, c;
 
-void solve(vector<vector<int>>& board, int x_st, int x_ed, int y_st, int y_ed) {
-    if (x_st == x_ed && y_st == y_ed) {        
-        board[x_st][y_st] = count++;
+void solve(int x_st, int x_ed, int y_st, int y_ed, int& count) {
+    if (!(r + 1 >= x_st && r + 1 <= x_ed && c + 1 >= y_st && c + 1 <= y_ed)) {
+        count += (x_ed - x_st + 1) * (y_ed - y_st + 1);
         return;
     }
-    solve(board, x_st, (x_st + x_ed) / 2, y_st, (y_st + y_ed) / 2);
-    solve(board, (x_st + x_ed) / 2, x_ed, y_st, (y_st + y_ed) / 2);    
-    solve(board, x_st, (x_st + x_ed) / 2, (y_st + y_ed) / 2, y_ed);
-    solve(board, (x_st + x_ed) / 2, x_ed, (y_st + y_ed) / 2, y_ed);
+    if (x_st == x_ed && y_st == y_ed) {
+        if (r + 1 == x_st && c + 1 == y_st) {
+            cout << count << endl;
+            return;
+        }
+        count++;
+        return;
+    }
+    solve(x_st, (x_st + x_ed) / 2, y_st, (y_st + y_ed) / 2, count);
+    solve(x_st, (x_st + x_ed) / 2, (y_st + y_ed) / 2 + 1, y_ed, count);
+    solve((x_st + x_ed) / 2 + 1, x_ed, y_st, (y_st + y_ed) / 2, count);
+    solve((x_st + x_ed) / 2 + 1, x_ed, (y_st + y_ed) / 2 + 1, y_ed, count);
 }
 
 int main(void) {
-    int N, r, c;
-    cin >> N, r, c;
+    int N, count = 0;
+    cin >> N >> r >> c;
     int size = pow(N);
-    vector<vector<int>> board(size + 1, vector<int>(size + 1));
-    solve(board, 1, N, 1, N);
-    cout << board[r][c];
+    solve(1, size, 1, size, count);
     return 0;
 }
