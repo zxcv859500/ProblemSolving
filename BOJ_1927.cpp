3cdf525930103027ac3b0645579d3e59457a1ace
DESKTOP-G1TP7S0\조나단 <zxcv859500@naver.com>
2021-01-08 09:20:23 +0900

    BOJ_1927 최소 힙

---
+++ BOJ_1927.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<queue>
+#include<functional>
+
+using namespace std;
+
+int main(void) {
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int n;
+    cin >> n;
+
+    while (n--) {
+        int num;
+        scanf("%d", &num);
+
+        if (num != 0) {
+            pq.push(num);
+        } else {
+            if (pq.size() == 0) {
+                printf("a 0\n");
+            } else {
+                int temp = pq.top();
+                printf("a %d\n", temp);
+                pq.pop();
+            }
+        }
+    }
+    return 0;
+}
